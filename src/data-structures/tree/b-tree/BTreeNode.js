54432b46b8bd205bcaed9b215634cb3be2b8f87b
hariv <vngpln@yahoo.co.in>
2018-07-03 00:23:26 -0700

    Added tree node traversal

--- src/data-structures/tree/b-tree/BTreeNode.js
+++ src/data-structures/tree/b-tree/BTreeNode.js
@@ -6,4 +6,19 @@ export default class BTreeNode {
     this.children = new Array(2 * this.degree);
     this.numKeys = 0;
   }
+
+  traversal() {
+    let i;
+    let traverse = [];
+    for (i = 0; i < this.numKeys; i += 1) {
+      if (!this.isLeaf) {
+        traverse = traverse.concat(this.children[i].traversal());
+      }
+      traverse.push(this.keys[i]);
+    }
+    if (!this.isLeaf) {
+      traverse = traverse.concat(this.children[i].traversal());
+    }
+    return traverse;
+  }
 }

00b52b4f71d3b0347b97115fbbe5fd7760dae010
hariv <vngpln@yahoo.co.in>
2018-07-03 02:16:58 -0700

    Added insert method

--- src/data-structures/tree/b-tree/BTree.js
+++ src/data-structures/tree/b-tree/BTree.js
@@ -1,6 +1,30 @@
+import BTreeNode from './BTreeNode';
+
 export default class BTree {
   constructor(degree = 0) {
     this.root = null;
     this.degree = degree;
+  }
+
+  insert(value) {
+    if (this.root === null) {
+      this.root = new BTreeNode(this.degree, true);
+      this.root.keys[0] = value;
+      this.root.numKeys = 1;
+    } else {
+      if (root.numKeys === (2 * this.degree) - 1) {
+        let i = 0;
+        const newRoot = new BTreeNode(this.degree, false);
+        newRoot.children[0] = this.root;
+        newRoot.splitChild(0, this.root);
+        if (newRoot.keys[0] < value) {
+          i += 1;
+        }
+        newRoot.children[i].insert(value);
+        this.root = newRoot;
+      } else {
+        this.root.insert(value);
+      }
+    }
   }
 }
--- src/data-structures/tree/b-tree/BTreeNode.js
+++ src/data-structures/tree/b-tree/BTreeNode.js
@@ -10,6 +10,10 @@ export default class BTreeNode {
     this.nodeComparator = new Comparator();
   }
 
+  setKeys(value) {
+    this.numKeys = value;
+  }
+
   traversal() {
     let i;
     let traverse = [];
@@ -37,5 +41,52 @@ export default class BTreeNode {
       return null;
     }
     return this.children[i].find(value);
+  }
+
+  insert(value) {
+    let i = this.numKeys - 1;
+    if (this.isLeaf) {
+      while (i >= 0 && this.keys[i] > value) {
+        this.keys[i + 1] = this.keys[i];
+        i -= 1;
+      }
+      this.keys[i + 1] = value;
+      this.numKeys += 1;
+    } else {
+      while (i >= 0 && this.keys[i] > value) {
+        i -= 1;
+      }
+      if (this.children[i + 1].numKeys === (2 * this.degree) - 1) {
+        this.splitChild(i + 1, this.children[i + 1]);
+        if (this.keys[i + 1] < value) {
+          i += 1;
+        }
+      }
+      this.children[i + 1].insert(value);
+    }
+  }
+
+  splitChild(i, node) {
+    const temp = new BTreeNode(node.degree, node.isLeaf);
+    let j;
+    temp.numKeys = this.degree - 1;
+    for (j = 0; j < this.degree - 1; j += 1) {
+      temp.keys[j] = node.keys[j + this.degree];
+    }
+    if (!node.isLeaf) {
+      for (j = 0; j < this.degree; j += 1) {
+        temp.children[j] = node.children[j + this.degree];
+      }
+    }
+    node.setKeys(this.degree - 1);
+    for (j = this.numKeys; j >= i + 1; j -= 1) {
+      this.children[j + 1] = this.children[j];
+    }
+    this.children[i + 1] = temp;
+    for (j = this.numKeys - 1; j >= i; j -= 1) {
+      this.keys[j + 1] = this.keys[j];
+    }
+    this.keys[i] = node.keys[this.degree - 1];
+    this.numKeys = this.numKeys + 1;
   }
 }

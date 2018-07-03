fedf84d18f0dc19d83aa82c848aefd3aea6f5a53
hariv <vngpln@yahoo.co.in>
2018-07-03 00:31:51 -0700

    Added BTreeNode find method

--- src/data-structures/tree/b-tree/BTreeNode.js
+++ src/data-structures/tree/b-tree/BTreeNode.js
@@ -1,3 +1,5 @@
+import Comparator from '../../../utils/comparator/Comparator';
+
 export default class BTreeNode {
   constructor(degree = 0, isLeaf = true) {
     this.degree = degree;
@@ -5,6 +7,7 @@ export default class BTreeNode {
     this.keys = new Array((2 * this.degree) - 1);
     this.children = new Array(2 * this.degree);
     this.numKeys = 0;
+    this.nodeComparator = new Comparator();
   }
 
   traversal() {
@@ -20,5 +23,19 @@ export default class BTreeNode {
       traverse = traverse.concat(this.children[i].traversal());
     }
     return traverse;
+  }
+
+  find(value) {
+    let i = 0;
+    while (i < this.numKeys && value > this.keys[i]) {
+      i += 1;
+    }
+    if (this.nodeComparator.equal(value, this.keys[i])) {
+      return this;
+    }
+    if (this.isLeaf) {
+      return null;
+    }
+    return this.children[i].find(value);
   }
 }
